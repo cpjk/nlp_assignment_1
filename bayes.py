d04d05841828c6ca0c76278ec3897ad2a8f06b3f
Chris Kelly <cpjkelly@gmail.com>
2017-05-20 18:48:32 -0700

    Group examples by class

--- bayes.py
+++ bayes.py
@@ -21,8 +21,8 @@ class MyBayesClassifier():
         """
         alpha_smooth = self._smooth
 
-        classes, class_occurrences = np.unique(y, return_counts=True)
-        class_occurrences = dict(zip(classes, class_occurrences))
+        classes, class_counts = np.unique(y, return_counts=True)
+        class_counts = dict(zip(classes, class_counts))
 
         num_classes, num_features = len(classes), X.shape[1]
 
@@ -32,34 +32,41 @@ class MyBayesClassifier():
         self._feat_prob = np.zeros((num_classes, num_features))
         self._class_prob = np.zeros(num_classes)
 
-        # get total num of examples
-        # for each class, get number of examples having that class
-        class_counts = np.unique(y, return_counts=True)[1]
-        # for 
+        # get total num of examples. done
 
+        # for each class, get number of examples having that class
+        # class_counts = np.unique(y, return_counts=True)[1]
+
+        # for each class, (get all examples in class)
+        #   for each word in vocabulary
+        #     get the number of examples that the word occurs in, for that class
+        #     (sum all columns of the examples for the class
+
+        pdb.set_trace()
+
+        ex_for_class = {}
+        for cls in classes:
+            # array of examples of that class, each of len num_feat
+            ex_for_class[cls] = np.zeros((class_counts[cls], num_features))
+
+            np_idx = 0
+            for idx, ex in enumerate(X):
+                if y[idx] == cls:
+                    ex_for_class[cls][np_idx] = ex
+                    np_idx += 1
+        pdb.set_trace()
+
+        class_examples = np.zeros
+        # for each class k, (get all examples in class)
+        #   for each word wt in vocabulary
+        #     estimate P(wt | C=k)
+
+        # for each class k, get P(C=k)
 
         # - For each class y, for each feature x, find P(x=0|y). note: P(x=1|y) = 1 - P(x=0|y)
         # and store in self._feat_prob[class][feat]
         # then P(y|X[i]) = self._class_prob[y] * mult(self._feat_prob[y]
 
-    # def train(self, X, y):
-    #     """
-    #     X is a document-term matrix.
-    #     y is an array of classes where y[0] is class of X[0]
-    #     """
-    #     alpha_smooth = self._smooth
-    #     classes, class_occurrences = np.unique(y, return_counts=True)
-    #     num_classes, num_features = len(classes), X.shape[1]
-    #     num_examples = X.shape[0]
-    #     self._num_classes, self._num_features = num_classes, num_features
-    #     self._feat_prob = np.zeros((num_classes, num_features))
-    #     self._class_prob = np.zeros(num_classes)
-
-    #     __calc_class_probs(classes, class_occurrences, num_examples)
-
-    #     # - For each class y, for each feature x, find P(x=0|y). note: P(x=1|y) = 1 - P(x=0|y)
-    #     # and store in self._feat_prob[class][feat]
-    #     # then P(y|X[i]) = self._class_prob[y] * mult(self._feat_prob[y]
 
     # for each feature vector in X, the corresponding index in pred should be
     # 0 if positive, 1 if negative
@@ -73,11 +80,11 @@ class MyBayesClassifier():
         # please leave this intact, we will use it for marking
         return self._class_prob, self._feat_prob
 
-    def __calc_class_probs(self, classes, class_occurrences, num_examples):
-        # - Find prob of each class P(y), store in self._class_prob[classes.index_of(y)]
-        #   P(y) = num_occurrences_of_y / num_examples
-        for klass, index in classes:
-            self._class_prob[index] = class_occurrences[index] / num_examples
+    # def __calc_class_probs(self, classes, class_occurrences, num_examples):
+    #     # - Find prob of each class P(y), store in self._class_prob[classes.index_of(y)]
+    #     #   P(y) = num_occurrences_of_y / num_examples
+    #     for cls, index in classes:
+    #         self._class_prob[index] = class_occurrences[index] / num_examples
 
 
 
@@ -116,7 +123,6 @@ X_train = vectorizer.fit_transform(data_train).toarray()
 X_test = vectorizer.transform(data_test).toarray()
 
 feature_names = vectorizer.get_feature_names()
-pdb.set_trace()
 
 clf = MyBayesClassifier(1)
 clf.train(X_train, y_train)
